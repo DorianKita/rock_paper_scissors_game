d9dbc8a42013ec7932ec639a7295e8daf5c036a5
Dorian Kita <dorian.kita90@gmail.com>
2023-06-05 20:31:30 +0200

    Added aiChoice function

--- app.js
+++ app.js
@@ -11,13 +11,29 @@ const game = {
 };
 
 const hands = [...document.querySelectorAll(".select img")];
+const btn = document.querySelector(".start");
 
 function handSelection() {
     hands.forEach(hand => hand.style.boxShadow = "")
     game.playerHand = this.dataset.option;
     this.style.boxShadow = "0 0 0 6px pink";
-}
+};
 
 hands.forEach(hand => {
     hand.addEventListener("click", handSelection)
-})
+});
+
+function aiChoice() {
+    const index = Math.floor(Math.random() * 3)
+    const aiHand = hands[index].dataset.option
+    console.log(aiHand)
+    return aiHand
+};
+
+function startGame() {
+    if (!game.playerHand) return alert("Choose your hand!")
+
+    game.aiHand = aiChoice()
+};
+
+btn.addEventListener("click", startGame);
